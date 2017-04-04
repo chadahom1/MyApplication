a50cc2e46f9e8d0614e629714215d0da1da4a185
Tosaphol Homchan <tosaphol.h@entronica.co.th>
2017-04-04 16:01:09 +0700

    initailview

--- app/src/main/java/com/example/suchada/myapplication/MainActivity.java
+++ app/src/main/java/com/example/suchada/myapplication/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.suchada.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit ประกาศตัวแปร
+    private EditText userEditText ,  passwordEditText;
+    private TextView textView;
+    private Button button;
+
+
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initial view คือการผูกตัวแปรอยู่ที่ View กับ Activity
+        initialView();
+
+
+
+    } // Main Method
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnlogin);
+
+
     }
-}
+
+
+} //Main Class คือ คลาสหลัก
