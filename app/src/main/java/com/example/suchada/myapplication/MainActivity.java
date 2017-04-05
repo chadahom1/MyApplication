f3db158e8ebb637e216f2be426ff74673ada53f2
Tosaphol Homchan <tosaphol.h@entronica.co.th>
2017-04-05 10:41:00 +0700

    Get Event From Click OK

--- app/src/main/java/com/example/suchada/myapplication/MainActivity.java
+++ app/src/main/java/com/example/suchada/myapplication/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.suchada.myapplication;
 
+import android.content.Intent;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit ประกาศตัวแปร
-    private EditText userEditText ,  passwordEditText;
+    private EditText userEditText, passwordEditText;
     private TextView textView;
     private Button button;
-
-
 
 
     @Override
@@ -25,9 +29,17 @@ public class MainActivity extends AppCompatActivity {
         //initial view คือการผูกตัวแปรอยู่ที่ View กับ Activity
         initialView();
 
+        //Create Controller
+        contoller();
 
 
     } // Main Method
+
+    private void contoller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
+
+    }
 
     private void initialView() {
         userEditText = (EditText) findViewById(R.id.edtUser);
@@ -37,6 +49,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @Override
+    public void onClick(View view) {
+        String tag = "SriwattanaV1";
+
+        //For TextView
+        if (view== textView) {
+            Log.d(tag,"You Click TextView");
+
+        }
+        // For Button
+        if (view == button) {
+            Log.d(tag, "You Click Button");
+        }
+
+    } // on click
 
 
 } //Main Class คือ คลาสหลัก
